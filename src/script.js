8aea6d32c99287efb536b868a105f706c23ef52a
Alexander Somma <admin@openco.ca>
2021-11-01 08:56:54 -0400

    Refactoring less and css

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function alpineInstance() {
+    return {
+      title: "Speakers",
+      intro:
+        'Grabbing Speakers list via <code>fetch()</code> request to render a list of items using Alpine.js :)',
+      users: []
+    };
+  }
+  
